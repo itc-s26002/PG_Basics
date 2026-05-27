205d444e582c81afcf4f41ff95c785560a4862ab
itc-s26002 <s26002@std.it-college.ac.jp>
2026-05-27 07:53:25 +0000

    例題13から26まで

---
+++ Chapter03/ex3-24.py
@@ -0,0 +1,19 @@
+home = "タイ"
+if home == "日本":
+    print("Hello, Japan!")
+elif home == "タイ":
+    print("Hello, Thailand!")
+elif home == "インド":
+    print("Hello, India!")
+elif home == "中国":
+    print("Hello, China!")
+else:
+    print("Hello, World!")
+
+
+
+
+
+
+
+
